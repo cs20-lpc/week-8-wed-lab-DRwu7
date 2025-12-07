2dc4127646bb6489376bf6a8fc7fd326f8e4022f
Chris <rjwu666@gmail.com>
2025-12-07 05:51:59 -0800

    update

--- lab10.cpp
+++ lab10.cpp
@@ -1,5 +1,6 @@
 #include "ArrayStack.hpp"
 #include <iostream>
+#include <string> // Added for clarity, though generally available via iostream
 
 using namespace std;
 
@@ -15,11 +16,9 @@ double gStack(unsigned);
  * Starting point of the program. Calls two functions in two different ways:
  * recursive and iterative. The outputs for both function calls should match
  * exactly.
- * 
- * Input:
+ * * Input:
  * N/A
- * 
- * Output:
+ * * Output:
  * An integer to signal to the OS the exit code.
 *******************************************************************************/
 
@@ -42,11 +41,9 @@ int main() {
  * Description:
  * Recursive function that calculates a result. Displays a message when a base
  * case is reached.
- * 
- * Input:
+ * * Input:
  * i - an unsigned value that determines whether or not this is a base case
- * 
- * Output:
+ * * Output:
  * The result of 1.1n + 3.2, where n is the depth of recursion
 *******************************************************************************/
 
@@ -63,14 +60,36 @@ double gRec(unsigned i) {
  * Description:
  * Iterative function that simulates recursion using a stack. Calculates a
  * result. Displays a message when a base case is reached.
- * 
- * Input:
+ * * Input:
  * i - an unsigned value that determines whether or not this is a base case
- * 
- * Output:
+ * * Output:
  * The result of 1.1n + 3.2, where n is the depth of recursion
 *******************************************************************************/
 
 double gStack(unsigned i) {
-    // TODO
+    // 1. Setup the stack (ArrayStack holds the recursion state variable 'i')
+    ArrayStack<unsigned> callStack(i + 1); // Max depth is i, so size is i+1 (for i=0)
+    
+    // 2. Unwind Phase: Simulate recursive calls by pushing 'i' onto the stack
+    unsigned currentValue = i;
+    while (currentValue > 0) {
+        // Push the state (the depth i) onto the stack
+        callStack.push(currentValue);
+        currentValue--;
+    }
+    
+    // 3. Base Case: Reached when currentValue is 0.
+    cout << "Base case!\n";
+    double result = 3.2; // The base case value
+    
+    // 4. Wind Phase: Calculate results by popping the stack
+    while (!callStack.isEmpty()) {
+        // Pop the state variable (not strictly needed, but mimics the return pop)
+        callStack.pop(); 
+        
+        // Apply the calculation step: f(i) = f(i-1) + 1.1
+        result += 1.1;
+    }
+    
+    return result;
 }
